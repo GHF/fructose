54388bb9672b59a2aeacc27d799b56405419bd29
Xo Wang <xo@geekshavefeelings.com>
2017-08-27 14:47:39 -0700

    Add echo to demo, refactor for style

--- main.cpp
+++ main.cpp
@@ -1,57 +1,53 @@
-/*
-    ChibiOS - Copyright (C) 2006..2016 Giovanni Di Sirio
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
+// Test program that blinks LEDs and echos data on main serial port.
+//
+// (C) Copyright 2017 Xo Wang <xo@geekshavefeelings.com>
+// SPDX-License-Identifier: Apache-2.0
 
 #include "ch.h"
 #include "hal.h"
-#include "ch_test.h"
 
-static THD_WORKING_AREA(waThreadBlink, 128);
-static THD_FUNCTION(ThreadBlink, arg) {
-  (void)arg;
+static const ioline_t kHeartbeatLed = LINE_LED_STAT;
+static const ioline_t kWarningLed = LINE_LED_WARN;
+static SerialDriver * const kMainSerial = &SD1;
 
-  chRegSetThreadName("Blink");
+static THD_WORKING_AREA(g_blink_wa, 128);
+static THD_FUNCTION(Blink, arg) {
+  (void) arg;
+
+  chRegSetThreadName(__func__);
   while (true) {
-    palClearLine(LINE_LED_STAT);
+    palClearLine(kHeartbeatLed);
     osalThreadSleepMilliseconds(500);
-    palSetLine(LINE_LED_STAT);
+    palSetLine(kHeartbeatLed);
     osalThreadSleepMilliseconds(500);
   }
 }
 
-/*
- * Application entry point.
- */
-int main(void) {
+static THD_WORKING_AREA(g_echo_wa, 256);
+static THD_FUNCTION(Echo, arg) {
+  SerialDriver * const serial_driver = static_cast<SerialDriver *>(arg);
 
-  /*
-   * System initializations.
-   * - HAL initialization, this also initializes the configured device drivers
-   *   and performs the board-specific initializations.
-   * - Kernel initialization, the main() function becomes a thread and the
-   *   RTOS is active.
-   */
+  chRegSetThreadName(__func__);
+  while (true) {
+    const char c = sdGet(serial_driver);
+    sdPut(serial_driver, c);
+  }
+}
+
+int main(void) {
   halInit();
   chSysInit();
 
-  chThdCreateStatic(waThreadBlink, sizeof(waThreadBlink), NORMALPRIO, ThreadBlink, NULL);
+  chThdCreateStatic(g_blink_wa, sizeof(g_blink_wa), LOWPRIO, Blink, nullptr);
+
+  sdStart(kMainSerial, nullptr);
+  chThdCreateStatic(g_echo_wa, sizeof(g_echo_wa), NORMALPRIO, Echo,
+                    kMainSerial);
 
   while (true) {
-    palClearLine(LINE_LED_WARN);
+    palClearLine(kWarningLed);
     osalThreadSleepMilliseconds(100);
-    palSetLine(LINE_LED_WARN);
+    palSetLine(kWarningLed);
     osalThreadSleepMilliseconds(900);
   }
 
