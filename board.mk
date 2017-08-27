fe494e57ac4571c90e0e70a68353d4ba439acdfc
Xo Wang <xo@geekshavefeelings.com>
2017-08-27 11:26:38 -0700

    Add initial files from F405 DISCOVERY C++ demo

---
+++ board.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+BOARDSRC = $(CHIBIOS)/os/hal/boards/OP_REVOLUTION/board.c
+
+# Required include directories
+BOARDINC = $(CHIBIOS)/os/hal/boards/OP_REVOLUTION
