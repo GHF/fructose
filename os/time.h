7eb6a97e177d2ad294951e5141475bffd510e4f5
Xo Wang <xo@geekshavefeelings.com>
2017-08-30 11:29:41 -0700

    Add semantics of infinite Duration
    Reserve the highest value of Duration representation to mean infinite
    time (e.g. for timeout specification).

--- os/time.h
+++ os/time.h
@@ -12,8 +12,9 @@
 #pragma once
 
 #include "base/integer.h"
-#include <cstdint>
+#include <algorithm>
 #include <limits>
+#include <cstdint>
 
 namespace Fructose {
 
@@ -44,7 +45,8 @@ class TimePoint {
   friend class Duration;
 };
 
-// Represents an interval of time. Can not be negative.
+// Represents an interval of time. Can not be negative. The maximum value is
+// reserved to represent infinite duration (e.g. for timeout specifications).
 class Duration {
  public:
   using MicrosecondsType = uint32_t;
@@ -62,29 +64,59 @@ class Duration {
 
   // Rounds up.
   constexpr MicrosecondsType Microseconds() const {
+    if (time_ == kInfinite) {
+      return std::numeric_limits<MicrosecondsType>::max();
+    }
     return ScaleRoundUp(time_, 1000000UL, kTimeResolution);
   }
   // Rounds up.
   constexpr MillisecondsType Milliseconds() const {
+    if (time_ == kInfinite) {
+      return std::numeric_limits<MillisecondsType>::max();
+    }
     return ScaleRoundUp(time_, 1000UL, kTimeResolution);
   }
   // Rounds up.
   constexpr SecondsType Seconds() const {
+    if (time_ == kInfinite) {
+      return std::numeric_limits<SecondsType>::max();
+    }
     return DivideRoundUp(time_, kTimeResolution);
   }
 
   // Construction functions.
   static constexpr Duration Microseconds(MicrosecondsType microseconds) {
-    return Duration(ScaleRoundUp(microseconds, kTimeResolution, 1000000UL));
+    const _TimeRepr time = std::min(kMaximum,
+        ScaleRoundUp(microseconds, kTimeResolution, 1000000UL));
+    return Duration(time);
   }
   static constexpr Duration Milliseconds(MillisecondsType milliseconds) {
-    return Duration(ScaleRoundUp(milliseconds, kTimeResolution, 1000UL));
+    const _TimeRepr time = std::min(kMaximum,
+        ScaleRoundUp(milliseconds, kTimeResolution, 1000UL));
+    return Duration(time);
   }
   static constexpr Duration Seconds(SecondsType seconds) {
-    return Duration(seconds * kTimeResolution);
+    const _TimeRepr time = std::min(kMaximum, seconds * kTimeResolution);
+    return Duration(time);
+  }
+
+  // Aliases for durations.
+  static constexpr Duration Immediate() {
+    return Duration(kImmediate);
+  }
+  static constexpr Duration Infinite() {
+    return Duration(kInfinite);
+  }
+  static constexpr Duration Max() {
+    return Duration(kMaximum);
   }
 
  private:
+  static constexpr _TimeRepr kImmediate = 0;
+  static constexpr _TimeRepr kInfinite =
+      std::numeric_limits<_TimeRepr>::max();
+  static constexpr _TimeRepr kMaximum = kInfinite - 1;
+
   explicit constexpr Duration(_TimeRepr time)
       : time_(time) {
   }
