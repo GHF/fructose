493ffc5811830b5cf4093ae1d33e69fa928ec725
Xo Wang <xo@geekshavefeelings.com>
2017-08-31 12:42:46 -0700

    Add reset thread to demo and switch to legacy logging

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "ch.h"
 #include "hal.h"
 
+#include "app/log.h"
 #include "version/version.h"
 #include "driver/mcp4725.h"
 #include "driver/mpu6000.h"
@@ -43,6 +44,36 @@ static THD_FUNCTION(Blink, arg) {
   }
 }
 
+static THD_WORKING_AREA(g_reset_watch_wa, 128);
+// Resets the system if two ^C characters are received in quick succession.
+// Also performs reception echo.
+static THD_FUNCTION(WatchForReset, arg) {
+  (void) arg;
+
+  chRegSetThreadName(__func__);
+  bool etx_received = false;
+  while (true) {
+    const msg_t msg = chnGetTimeout(kMainSerial, MS2ST(500));
+    if (msg == Q_TIMEOUT || msg == Q_RESET) {
+      etx_received = false;
+      continue;
+    }
+    const uint8_t c = msg;
+    if (c == '\x03') {
+      if (etx_received) {
+        break;
+      } else {
+        etx_received = true;
+      }
+    } else {
+      etx_received = false;
+    }
+    // Echo character back.
+    sdPut(kMainSerial, c);
+  }
+  NVIC_SystemReset();
+}
+
 int main(void) {
   halInit();
   chSysInit();
@@ -50,14 +81,17 @@ int main(void) {
   chThdCreateStatic(g_blink_wa, sizeof(g_blink_wa), LOWPRIO, Blink, nullptr);
 
   sdStart(kMainSerial, nullptr);
-  printf("\r\nBoard \"%s\" (%s built on %s)\r\n", BOARD_NAME, g_build_version,
-         g_build_time);
+  chThdCreateStatic(g_reset_watch_wa, sizeof(g_reset_watch_wa), HIGHPRIO,
+                    WatchForReset, nullptr);
+  puts("");
+  LogInfo("Board \"%s\" (%s built on %s)", BOARD_NAME,
+          g_build_version, g_build_time);
 
   spiStart(kMpuSpi, &kMpuSpiConfig);
   ChibiOsSpiMaster mpu_spi_master(kMpuSpi);
   ChibiOsSpiDevice mpu_spi_device(kMpuSpiCs);
   Mpu6000 mpu6000(&mpu_spi_master, &mpu_spi_device);
-  printf("MPU-6000 detect: %u\r\n", mpu6000.Detect());
+  LogDebug("MPU-6000 detect: %u", mpu6000.Detect());
   mpu6000.ResetDevice();
   mpu6000.SetupDevice(Mpu6000::CONFIG__DLPF_CFG__5_HZ,
                       Mpu6000::GYRO_CONFIG__FS_SEL__1000_DPS,
